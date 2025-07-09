ed132bc0ef39a174ebc1a42a78ea5bcf9eeae398
yasmintuck <74052398+yasmintuck@users.noreply.github.com>
2025-07-09 23:21:36 +0100

    Update Dashboard.jsx

--- frontend/frontend/src/pages/Dashboard.jsx
+++ frontend/frontend/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect } from "react";
 import ModuleLoader from "../components/ModuleLoader";
 import { useState } from "react";
+import "../index.css";
 
 function Dashboard() {
   const { user, isAuthenticated, isLoading, logout } = useAuth0();
