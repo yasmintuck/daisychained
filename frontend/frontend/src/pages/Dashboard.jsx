0a402af11bcfde04be16ff7af6419c3350562810
yasmintuck <74052398+yasmintuck@users.noreply.github.com>
2025-07-10 21:42:22 +0100

    reverting back

--- frontend/frontend/src/pages/Dashboard.jsx
+++ frontend/frontend/src/pages/Dashboard.jsx
@@ -1,20 +1,14 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import ModuleLoader from "../components/ModuleLoader";
+import { useState } from "react";
 import { Navigate } from "react-router-dom";
 import './Dashboard.css';
 
 function Dashboard() {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
-  if (isLoading) {
-    return (
-      <div className="spinner-wrapper">
-        <img src="/logo.png" alt="Loading..." className="spinner" />
-      </div>
-    );
-  }
-
+  if (isLoading) return <p style={{ padding: "2rem" }}>Loading...</p>;
   if (!isAuthenticated) return <Navigate to="/" />;
 
   const email = user?.email || "";
