5fea4192c0d5f283944ada58c44193a83151ccec
yasmintuck <74052398+yasmintuck@users.noreply.github.com>
2025-07-10 21:38:54 +0100

    spinner adjustments

--- frontend/frontend/src/pages/Dashboard.jsx
+++ frontend/frontend/src/pages/Dashboard.jsx
@@ -1,14 +1,20 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ModuleLoader from "../components/ModuleLoader";
-import { useState } from "react";
 import { Navigate } from "react-router-dom";
 import './Dashboard.css';
 
 function Dashboard() {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
-  if (isLoading) return <p style={{ padding: "2rem" }}>Loading...</p>;
+  if (isLoading) {
+    return (
+      <div className="spinner-wrapper">
+        <img src="/logo.png" alt="Loading..." className="spinner" />
+      </div>
+    );
+  }
+
   if (!isAuthenticated) return <Navigate to="/" />;
 
   const email = user?.email || "";
