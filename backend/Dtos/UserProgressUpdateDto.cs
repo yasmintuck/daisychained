4a9673c237d9bc00a9027e2d00e6121509a81794
yasmintuck <74052398+yasmintuck@users.noreply.github.com>
2025-07-14 16:23:46 +0100

    fixing user progress tracking

--- backend/Dtos/UserProgressUpdateDto.cs
+++ backend/Dtos/UserProgressUpdateDto.cs
@@ -2,7 +2,7 @@ namespace backend.Dtos
 {
 public class UserProgressUpdateDto
 {
-    public int UserId { get; set; }
+    public string UserExternalId { get; set; }
     public int ModuleId { get; set; }
     public int Progress { get; set; }
 }
