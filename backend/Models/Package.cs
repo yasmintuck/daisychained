5c394870bcc4a7ccdb91ac1648e77cbdf5f0c254
yasmintuck <74052398+yasmintuck@users.noreply.github.com>
2025-10-10 13:12:57 +0100

    add colour to package

--- backend/Models/Package.cs
+++ backend/Models/Package.cs
@@ -5,6 +5,7 @@ public class Package
     public int PackageId { get; set; }
     public string PackageName { get; set; }
     public string PackageDescription { get; set; }
+    public string? ColorHex { get; set; }
 
     public ICollection<ModulePackage> ModulePackages { get; set; } = new List<ModulePackage>();
     public ICollection<OrganisationPackage> OrganisationPackages { get; set; } = new List<OrganisationPackage>();
